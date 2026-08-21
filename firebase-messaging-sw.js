b6921f9aac5e5efc5f40c9326862db7741723872
Snsn5450 <snsnmaint@gmail.com>
2026-08-21 22:43:00 +0530

    Refactor Firebase messaging service worker
    Refactored Firebase messaging service worker code for clarity and efficiency.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,27 +1,6 @@
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js"
-);
-
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js"
-);
-
-firebase.initializeApp({
-  apiKey: "AIzaSyDJVPZFRkzWKJ58cyIfaiuPqdvY4cYYzvE",
-  authDomain: "sns-maintenance-pwa.firebaseapp.com",
-  projectId: "sns-maintenance-pwa",
-  storageBucket: "sns-maintenance-pwa.firebasestorage.app",
-  messagingSenderId: "683218813920",
-  appId: "1:683218813920:web:a091496adb477c47502185"
-});
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function(payload) {
-
-  console.log(
-    "SNS background push:",
-    payload
-  );
-
-});
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
+firebase.initializeApp({apiKey:"AIzaSyDJVPZFRkzWKJ58cyIfaiuPqdvY4cYYzvE",authDomain:"sns-maintenance-pwa.firebaseapp.com",projectId:"sns-maintenance-pwa",storageBucket:"sns-maintenance-pwa.firebasestorage.app",messagingSenderId:"683218813920",appId:"1:683218813920:web:a091496adb477c47502185"});
+const messaging=firebase.messaging();
+messaging.onBackgroundMessage(function(payload){console.log("SNS background FCM",payload);});
+self.addEventListener("notificationclick",e=>{e.notification.close();e.waitUntil(clients.openWindow("./"));});
